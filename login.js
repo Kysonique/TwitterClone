92f44dbc669d2ba9111c71b23e457cbacc69f976
Kysonique <Kyras.bradley@gmail.com>
2023-01-27 00:33:56 -0500

    saving

--- login.js
+++ login.js
@@ -2,90 +2,106 @@ const USER_URL = "https://twtclonerevamp-default-rtdb.firebaseio.com/users";
 const EXT = ".json"
 
 //signup page
-const userInputs = document.querySelectorAll('form-input');
+const userInputs = document.querySelectorAll('form-control form-control-lg');
 const submitUser = document.getElementById('submitUserBtn');
-
 const password1 = document.getElementById('password-input');
 const password2 = document.getElementById('password-ReEnter');
+// add event listener to each field on the form
+const nuFirstName = document.getElementById('firstName-input'); 
+const nuLastName = document.getElementById('lastName-input'); 
+const nuBirthday = document.getElementById('birthday-input'); 
+const nuEmail = document.getElementById('email-input'); 
+const nuDisplayName = document.getElementById('username-input'); 
+const nuPhoneNumber = document.getElementById('phoneNumber-input'); 
+const nuPassword = document.getElementById('password-input'); 
+//reading the data from the form
+function form_DATA(){
+    let USER_Obj = {
+        firstName: nuFirstName.value,
+        lastName: nuLastName.value,
+        birthday: nuBirthday.value,
+        emailAddress: nuEmail.value,
+        phoneNumber: nuPhoneNumber.value,
+        userName: nuDisplayName.value,
+        password: nuPassword,
+        uuid: gennerateUID(),
+    }
+    return USER_Obj;
+}
+// let new_USER;
+function saveUserInfo(user){
+        fetch(`${USER_URL}${EXT}`,{
+        method: "post",
+        headers:{
+            'content-type':'application/json'
+        },
+        body: JSON.stringify({...user}),
+    })
+        .then((resp) =>{
+            resp.json();
+            console.log('first then')
+        })
+            .then((data) => console.log('data', data));
+} console.log(JSON.parse(user))
 
+function formRESET(){
+    nuFirstName.value = "";
+    nuLastName.value = "";
+    nuBirthday.value = "";
+    nuEmail.value = "";
+    nuDisplayName.value = "";
+    nuPhoneNumber.value = "";
+    nuPassword.value = "";
 
-// submitUser.addEventListener('click', (event) =>{
+}
 
-// })
-// function passwordCheck(){
-//     if(password1.value /= password2.value){
-//         alert("password doesn't match");
-//     }
-// }
+let newWindow = (window.location.href='login.html')
+function handleSubmitBtn(){
+    let savedUser = form_DATA();
+    saveUserInfo(savedUser)
+    formRESET();
+    alert('user created');window.location.href='login.html'
+}
+    
 
-// let inputValue;
-// function inputFields(){
-//     userInputs.forEach(inputDiv => {
-//         inputValue = Object.keys(inputDiv);
-//         console.log(inputValue)
-//         // if (!inputDiv.value > 0){
-//         //     alert('Invalid entry')
-//         // } 
-//     });
-// }
-
-// let USER_DATA;
-// function getUsers(){
-//     fetch(`${USER_URL}${EXT}`)
-//     .then((resp) => resp.json())
-//     .then((data) =>{
-//         USER_DATA = {...data};
-//         console.log(USER_DATA)
-//     });
-// }
-
-//Object.keys(USER_DATA).forEach(userID =>{ let user = USER_DATA[userID]})
-
+function gennerateUID(){
+    return Math.random().toString(15).substring(2,15)
+}
+//end of signup functions
 
 //login page
-const username = document.getElementById('userName');
-const password = document.getElementById('userPassword');
-
-const URL = "https://data.json";
-
-
-username.addEventListener('keyup', (event) =>{
-    console.log(username.value)
-});
-
-password.addEventListener('keyup', (event) =>{
-    console.log(password.value)
-});
-
 //user validation
-const loginBtn = document.getElementById('logInBtn');
+const userLoginName = document.getElementById('user-Name');
+const loginPassword = document.getElementById('user-Password');
 
-// let USER_DATA;
-// function getUsers(){
-//     fetch(`${USER_URL}${EXT}`)
-//     .then((resp) => resp.json())
-//     .then((data) =>{
-//         USER_DATA = {...data};
-//         console.log(USER_DATA)
-//     });
-// }
+const userValidation = (user) => new Promise((res,rej) =>{
+    setTimeout(() =>{
+        res(JSON.stringify.apply({user, status: 200, ok: true}))
+    },2000)
+})
 
-loginBtn.addEventListener('click', (event) =>{
-    let DATA_ARR;
-    fetch(`${USER_URL}${EXT}`)
-    .then((resp) => resp.json())
-    .then((data) =>{
-        DATA_ARR = {...data};
-        console.log(DATA_ARR)
-    });
+// const getUSERname =(user) => fetch(`${USER_URL}/${user.}`)
 
-    let USER_DATA = DATA_ARR
-    for(let i = 0; i < USER_DATA.length; i++){
-        if(USER_DATA[i].userName /= username.value){
-            alert("invalid user")
-        }
-    } window.location.href = "file:///C:/Users/kyras/OneDrive/Desktop/FED-2211%20Repos/TwitterClone/dashboard.html";
-
+userLoginName.addEventListener('keyup', (event) =>{
+    console.log(userLoginName.value)
 });
 
+const loginBtn = document.getElementById('logInBtn');
+// loginBtn.addEventListener('click', (event) =>{
+//    if (!loginPassword.value >= 5 || !userLoginName.value){
+//         alert('invalid user')
+//    } else{
+//         window.location.href = "file:///C:/Users/kyras/OneDrive/Desktop/FED-2211%20Repos/TwitterClone/dashboard.html"; 
+//    }
+
+// });
+loginPassword.addEventListener('keyup', (event) =>{
+    console.log(loginPassword.value)
+});
+// async function handleLoginBtn(event){
+//     event.preventDefault()
+//     event.stopPropagation()
+
+//     const 
+// }
 
--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+//user bio page
 //toggle btnx
 const userPostBtn = document.getElementById('btnradio1');
 const userImageBtn = document.getElementById('btnradio2');
 const imgDiv = document.getElementById('imagesDiv');
 const postDiv = document.getElementById('postcardSection');
 const USER_URL = "https://twtclonerevamp-default-rtdb.firebaseio.com/users";
+const EXT = ".json"
 
 
 let newUser;
@@ -15,6 +17,7 @@ function getUsers(){
         console.log(newUser)
     });
 }
+
 imgDiv.style.display = "none";
 userImageBtn.addEventListener('click', (event) =>{
     postDiv.style.display = "none";
@@ -24,8 +27,6 @@ userPostBtn.addEventListener('click', (event) =>{
     postDiv.style.display = "block"; 
     imgDiv.style.display = "none"
 });
-
-
 // edit bio (complete possibly can make it add to api)
 const bioParagraph = document.getElementById('bioParagraph');
 const editBtn = document.getElementById('edit-btn');
@@ -42,82 +43,32 @@ function editBio(){
     }
 }
 
-
 //create new post (need to append to child and add to api)
-const POST_URL = "https://twtclonerevamp-default-rtdb.firebaseio.com/posts";
-const EXT = ".json";
-
-
 //const postCard = document.getElementById('postcard');
-let userPosts = {};
-function getPosts(){
-    fetch(`${POST_URL}${EXT}`)
-    .then((resp) => resp.json())
-    .then((data) =>{
-        userPosts = {...data};
-        console.log('user posts', userPosts)
-    });
-}
+// let userPosts = {};
+// function getPosts(){
+//     fetch(`${POST_URL}${EXT}`)
+//     .then((resp) => resp.json())
+//     .then((data) =>{
+//         userPosts = {...data};
+//         console.log('user posts', userPosts)
+//     });
+// }
+
+
+
 
 
 //new post
  //submit button for post
- const newMessge = document.getElementById('newMessage');
+const newMessge = document.getElementById('newMessage');
 const messageSubmit = document.getElementById('msgBtn');
-
 
 let newpost;
 newMessge.addEventListener('keyup', (event) =>{
    console.log(newMessge.value)
    newpost = newMessge.value;
 });
-
-
-
-//const postCardDiv = document.getElementById('postcard');
-// let addNewPost = document.createElement('div');
-// addNewPost.classList = ('postcard')
-// postDiv.innerHTML = `
-// <div class="card" style="width: 18rem">
-//   <div class="card-body">
-//     <img
-//       src="C:/Users/kyras/OneDrive/Pictures/opalLogop.png"
-//       class="card-img-top"
-//       alt="..."
-//     />
-//     <h5 class="card-title">Display Name</h5>
-//     <h6 class="card-subtitle mb-2 text-muted">@Username</h6>
-//     <div id="cardDiv"><input type="input" id="card-text" class="card-text" value="${newpost}"></div>
-//     <button id="deletePost"
-//         type="button"
-//         class="btn btn-outline-primary"
-//         style="
-//           --bs-btn-padding-y: 0.25rem;
-//           --bs-btn-padding-x: 0.5rem;
-//           --bs-btn-font-size: 0.75rem;">delete
-        
-//     </button>
-//     <button id="editPost"
-//         type="button"
-//         class="btn btn-outline-primary"
-//         style="
-//           --bs-btn-padding-y: 0.25rem;
-//           --bs-btn-padding-x: 0.5rem;
-//           --bs-btn-font-size: 0.75rem;" >edit
-        
-//     </button>
-//   </div>  
-// </div>`
-
-// const newMessge = document.getElementById('newMessage');
-// const messageSubmit = document.getElementById('msgBtn');
-
-
-// let newpost;
-// newMessge.addEventListener('keyup', (event) =>{
-//    console.log(newMessge.value)
-//    newpost = newMessge.value;
-// });
 
 messageSubmit.addEventListener('click', (event) =>{
     let addNewPost = document.createElement('div');
@@ -132,8 +83,8 @@ addNewPost.classList = ('postcard')
       alt="..."
     />
     <h5 class="card-title">Display Name</h5>
-    <h6 class="card-subtitle mb-2 text-muted">@Username</h6>
-    <div id="cardDiv"><input type="input" id="card-text" class="card-text" value="${newpost}"></div>
+    <h6 class="card-subtitle mb-2 text-muted">@username</h6>
+    <div id="cardDiv"><input type="input" id="card-text" class="card-text" value="${newpost}" readonly></div>
     <button id="deletePost"
         type="button"
         class="btn btn-outline-primary"
@@ -143,7 +94,7 @@ addNewPost.classList = ('postcard')
           --bs-btn-font-size: 0.75rem;">delete
         
     </button>
-    <button id="editPost"
+    <button id="edit-Post"
         type="button"
         class="btn btn-outline-primary"
         style="
@@ -158,53 +109,26 @@ addNewPost.classList = ('postcard')
    postDiv.prepend(addNewPost)
 });
 
-
-function postMessageToAPI(){
-    fetch(`${POST_URL}${EXT}`)
-    .then((resp) => resp.json())
-    .then((data) =>{
-        userPosts = {...data};
-        console.log('user posts', userPosts)
-    });
-
-};
-
 //edit post (need to update in api)
-const editPostBtn = document.getElementById('editPost');
-const postText = document.getElementById('card-text');
-// editPostBtn.innerHTML = 'edit';
-// postText.setAttribute('readonly', 'readonly')
+    const editPostBtn = document.getElementById('edit-Post');
+    const postText = document.getElementById('card-text');
+    
+    // postText.setAttribute('readonly', 'readonly')
+    // editPostBtn.addEventListener('click', editNewPost())   
 
     function editNewPost(){
         if (editPostBtn.innerHTML === 'edit'){
             editPostBtn.innerHTML = 'save';
             postText.removeAttribute('readonly');
-    
+
         } else{
             editPostBtn.innerHTML = 'edit';
             postText.setAttribute('readonly', 'readonly');
         }
     }
-// editPostBtn.addEventListener('click', (editNewPost()))
-//delete post
-	// // ** Delete Btn Functionality
-		// task_delete_el.addEventListener('click', () => {
-		// 	list_el.removeChild(task_el);
-		// });
 
 
 
-		// // ** Edit Btn Functionality
-		// task_edit_el.addEventListener('click', () => {
-		// 	if (task_edit_el.innerHTML.toUpperCase() === 'EDIT') {
-		// 		// task readonly attribute => remove
-		// 		task_input_el.removeAttribute('readonly');
-		// 		task_input_el.focus();
-		// 		task_edit_el.innerText = 'Save';
-		// 	} else {
-		// 		// make input readonly
-		// 		task_input_el.setAttribute('readonly', 'readonly');
-		// 		// change btn innerText back to edit
-		// 		task_edit_el.innerText = 'edit';
-		// 	}
-		// });
+
+
+// task_delete
